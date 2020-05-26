1de442f290dc4ec7b517f0c287d8e175375baf76
wintertee <27325188+wintertee@users.noreply.github.com>
2020-05-26 15:00:29 +0800

    REWRITE 1003

--- splits/splits-gen.py
+++ splits/splits-gen.py
@@ -2,8 +2,8 @@ import os
 path = os.getcwd()
 print(path)
 
-with open(path+r'/monodepth2/splits/eigen_full/val_files.txt','r') as s:
-    with open(path+r'/monodepth2/splits/1003/val_files.txt','w') as d:
+with open(path+r'/monodepth2/splits/eigen_zhou/train_files.txt','r') as s:
+    with open(path+r'/monodepth2/splits/1003/train_files.txt','w') as d:
         lines = s.readlines()
         for line in lines:
             if "10_03" in line:
